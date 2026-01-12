72a6388f0fc543ca2423db2f2f313f372acfd68e
Miroshkin <vovamiroshkin2011@gmail.com>
2026-01-12 21:50:50 +0300

    Рефакторинг title 1

--- title/script.js
+++ title/script.js
@@ -1,3 +1,11 @@
-function cont() {
+// Переход на главную
+function home() {
     window.location.href = "/main/"
 }
+
+// Единожды производим анимацию
+const text = document.querySelector("#main-text")
+text.classList.add("main-text-anim")
+text.addEventListener("animationend", () => {
+    text.classList.remove("main-text-anim")
+})
