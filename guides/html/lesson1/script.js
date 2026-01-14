9496bd8af43a3f76150bb8a0a09c0198abb9001b
Miroshkin <vovamiroshkin2011@gmail.com>
2026-01-14 21:47:55 +0300

    Добавление руководств

---
+++ guides/html/lesson1/script.js
@@ -0,0 +1,3 @@
+function back() {
+    window.location.href = "/guides/html.main"
+}
--- main/script.js
+++ main/script.js
@@ -1,3 +1,8 @@
+// Назад
 function back() {
     window.location.href = "/title/"
 }
+
+// Настройка границы списка
+const border = document.querySelector("#border")
+border.style.height = window.innerHeight - border.offsetTop - 22 + "px"
