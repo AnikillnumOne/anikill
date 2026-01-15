cbb97b3e3bb74b0679328e6b7ca153b65714e3a8
Miroshkin <vovamiroshkin2011@gmail.com>
2026-01-15 20:29:46 +0300

    Главная страница 2

--- main/script.js
+++ main/script.js
@@ -2,7 +2,3 @@
 function back() {
     window.location.href = "/title/"
 }
-
-// Настройка границы списка
-const border = document.querySelector("#border")
-border.style.height = window.innerHeight - border.offsetTop - 22 + "px"
